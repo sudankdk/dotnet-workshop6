ccd004106639f5243aa39ef3e27afee3adb9c38b
sudankdk <sudankhadka345@gmail.com>
2025-12-03 12:19:11 +0545

    task 2.1 done

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -1,15 +1,36 @@
 ﻿namespace Task_2;
 public delegate double Calculate(double a, double b);
 
+public delegate double DiscountStrategy(double price);
+
 class Program
 {
     static double sum(double a, double b)=> a+b;
     static double subtract(double a, double b) => a - b;
+
+    static double FestivalDiscount(double price) => 0.2 * price;
+    static double SeasonalDiscount(double price) => 0.1 * price;
+    static double NoDiscount(double price) => 0;
+
+    static double CalculateFinalPrice(double originalPrice, DiscountStrategy strategy)
+    {
+        var dis=strategy(originalPrice);
+        return originalPrice - dis;
+    }
+
+    
+    
     static void Main(string[] args)
     {
         Calculate s = sum;
         Console.WriteLine($"The sum is {s(1,2)}");
         Calculate c = subtract;
         Console.WriteLine($"The subtract is {c(10,2)}");
+
+        Console.WriteLine(CalculateFinalPrice(1000, FestivalDiscount));
+        Console.WriteLine(CalculateFinalPrice(1000, SeasonalDiscount));
+        Console.WriteLine(CalculateFinalPrice(1000, NoDiscount));
+        Console.WriteLine(CalculateFinalPrice(1000, (double x=10000)=>0.3*x));
+
     }
 }
--- task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net9.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7893b85fab6e6a1f0c72157562a1c362345182e")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
