62eb46b2777d1f813b7e9384cfb937a8f1bead4b
sudankdk <sudankhadka345@gmail.com>
2025-12-03 11:39:40 +0545

    task 1 done

---
+++ task1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Rectangle rectangle = new Rectangle();
+       var l= rectangle.Length = 20;
+        var b=rectangle.Breadth = 20;
+        double ans=rectangle.Area(l, b);
+        Console.WriteLine($"The area is {ans}");
+        double perimeter=rectangle.Perimeter(l, b);
+        Console.WriteLine($"The perimeter is {perimeter}");
+    }
+}
+
+
+class Rectangle
+{
+    private double length;
+    private double breadth;
+
+    public double Length
+    {
+        get => length;
+        set => length = value; 
+    }
+
+    public double Breadth
+    {
+        get => breadth;
+        set => breadth = value;
+    }
+
+
+
+   public  double Area(double l, double b) => l * b;
+
+   public  double Perimeter(double l, double b) =>l + 2 * b;
+  
+}
