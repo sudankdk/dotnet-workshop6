e7893b85fab6e6a1f0c72157562a1c362345182e
sudankdk <sudankhadka345@gmail.com>
2025-12-03 11:48:35 +0545

    task 2 done

---
+++ Task 2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_2;
+public delegate double Calculate(double a, double b);
+
+class Program
+{
+    static double sum(double a, double b)=> a+b;
+    static double subtract(double a, double b) => a - b;
+    static void Main(string[] args)
+    {
+        Calculate s = sum;
+        Console.WriteLine($"The sum is {s(1,2)}");
+        Calculate c = subtract;
+        Console.WriteLine($"The subtract is {c(10,2)}");
+    }
+}
